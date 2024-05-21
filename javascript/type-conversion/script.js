c71f7a072a6d8a810ac2db94031816556e3ef710
hyunjin4270 <hyunn4270@gmail.com>
2024-05-21 17:45:13 +0900

    type-conversion

---
+++ javascript/type-conversion/script.js
@@ -0,0 +1,21 @@
+let val
+
+// Number to String
+val = String(111);
+val = String(8 + 4);
+// Boolean to String
+val = String(false);
+// Date to String
+val = String(new Date());
+// Array to String
+val = String()[1, 2, 3, 4, 5];
+// toString()
+val = (5).toString();
+
+
+// String to Number
+val = Number('1');
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
