49f9180c3cdefbdfbcc21f9a80c070bf8b85927c
hyunjin4270 <hyunn4270@gmail.com>
2024-06-10 13:09:04 +0900

    Relearn Generic

---
+++ javascript/document/script.js
@@ -0,0 +1,19 @@
+let val;
+
+val = document;
+
+val = document.baseURI; // 웹 페이지의 절대 URI 반환
+val = document.head; //<head> 태그 반환
+val = document.body; //<body> 태그 반환
+
+
+val = document.forms; // <forms> 태그 반환
+val = document.forms[0].id;
+val = document.forms[0].classList;
+val = document.forms[0].className;
+
+val = document.scripts;
+
+console.log(val);
+
+const headerContainer = document.
