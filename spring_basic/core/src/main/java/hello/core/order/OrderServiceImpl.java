b534b3e458e3ea9caa420bd2f9769cc3c07ca18c
hyunjin4270 <hyunn4270@gmail.com>
2024-06-18 17:15:42 +0900

    studing Spring

--- spring_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring_basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,13 +7,18 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+
+    public OrderServiceImpl(DiscountPolicy discountPolicy, MemberRepository memberRepository) {
+        this.discountPolicy = discountPolicy;
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
