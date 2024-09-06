797d327ce816f182a4214e3792bc2bd4b86a28a6
hyunjin4270 <hyunn4270@gmail.com>
2024-09-06 12:04:08 +0900

    2024-09-05

--- ONEBITE-REACT/section05/src/App.jsx
+++ ONEBITE-REACT/section05/src/App.jsx
@@ -1,28 +1,20 @@
 import './App.css'
-import Header from "./components/Header";
-import Footer from './components/Footer';
-import Main from './components/Main';
-import Button from './components/Button';
+import HookExam from './components/HookExam';
+
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
-
+  
+  
+  
   return (
     <>
-    <Button {...buttonProps}/>
-    <Button text = {"카페"}/>
-    <Button text = {"블로그"}>
-      <div>자식요소</div>
-    </Button>
+      <HookExam />
     </>
-  )
-}
+    );
+  };
+
+
+
 
 
 export default App
---
+++ ONEBITE-REACT/section05/src/components/Counter.jsx
@@ -0,0 +1,22 @@
+import {useState} from 'react';
+
+const Counter = () => {
+    const [state, setState] = useState(0);
+  
+    return (
+      <>
+        <div>
+        <h1>{state}</h1>
+        <button
+          onClick = {() => {
+            setState(state + 1);
+          }}
+          >
+            +
+          </button>
+        </div>
+      </>
+    )
+  }
+
+export default Counter;
